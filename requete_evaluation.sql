983a06533edc95e696a36d3ee23edd6556041ff8
Khazza <mahetoquet@gmail.com>
2023-04-19 10:44:24 +0200

    debut evaluation northwind

---
+++ requete_evaluation.sql
@@ -0,0 +1,30 @@
+---- La base Northwind
+-- Ecrivez ensuite les requêtes permettant d'obtenir les résultats attendus suivants :
+-- 1- Liste des clients français : 
+
+
+-- 2- Liste des produits vendus par le fournisseur "Exotic Liquids" :
+
+
+-- 3- Nombre de produits mis à disposition par les fournisseurs français (tri par nombre de produits décroissant) : 
+
+
+-- 4- Liste des clients français ayant passé plus de 10 commandes : 
+
+
+-- 5- Liste des clients dont le montant cumulé de toutes les commandes passées est supérieur à 30000 € :
+
+
+-- 6- Liste des pays dans lesquels des produits fournis par "Exotic Liquids" ont été livrés : 
+
+
+-- 7- Chiffre d'affaires global sur les ventes de 1997 :
+
+
+-- 8- Chiffre d'affaires détaillé par mois, sur les ventes de 1997 : 
+
+
+-- 9- A quand remonte la dernière commande du client nommé "Du monde entier" ? 
+
+
+-- 10- Quel est le délai moyen de livraison en jours ? 
